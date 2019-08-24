d2da846b4cfbe34331afdd12363866c9651a0604
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-08-24 15:57:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  arr = ["item1", "item2", "item3"]
+  arr = ["item1", "item2", "item3", "arrays!"]
   arr.pop()
 end
 
