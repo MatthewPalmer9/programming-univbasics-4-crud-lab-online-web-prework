638b5ba66f9e104f32efba9fa64eeca7225560d8
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-08-24 15:57:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   arr = ["item1", "item2", "item3"]
-  arr.push("item4")
+  arr.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
