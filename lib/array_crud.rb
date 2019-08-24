34e3d0b028732a7015ce2fe53eb811c83764c5db
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-08-24 15:57:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  arr = ["item1", "item2", "item3"]
+  arr = ["item1", "item2", "item3", "item4"]
 end
 
 def add_element_to_end_of_array(array, element)
