1c700bb00e08c5d322cd8b8ae34bd672b81de26c
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-08-24 15:57:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   arr = ["item1", "item2", "item3"]
-  arr.unshift("item0")
+  arr.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
